52b50bed0621ef6b54bc39739b5e33f8a57fe050
leofmr <leonardo@moutinho.com.br>
2023-08-22 21:01:26 +0000

    Adicional isort

---
+++ test.py
@@ -0,0 +1,16 @@
+"""Test module
+"""
+import datetime
+import typing
+
+
+def print_text(text: str):
+    """print an text
+
+    Args:
+        text (str): text to print
+    """
+    print(text)
+
+
+print_text("2")
